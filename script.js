bff1cc22cc70438b4136eb69b3bbfad4962e3be1
Julia Melik <julija.melik@gmail.com>
2022-08-04 14:35:20 +0200

    doplnek

---
+++ script.js
@@ -0,0 +1,28 @@
+let client = new Paho.MQTT.Client(
+    'd57a0d1c39d54550b147b58411d86743.s2.eu.hivemq.cloud',
+    8884,
+    'printgo' + Math.random()
+)
+
+client = new Paho.MQTT.Client("d57a0d1c39d54550b147b58411d86743.s2.eu.hivemq.cloud", 8884, 'letni-skola' + Math.random())
+
+client.onConnectionLost = onConnectionLost;
+// client.onMessageArrived = onMessageArrived;
+
+client.connect({
+    onSuccess: onConnect,
+    userName: "robot",
+    password: "P@ssW0rd!",
+    useSSL: true
+});
+
+
+function onConnect() {
+    console.log("onConnect");
+    client.subscribe("/in/queue/count/#");
+}
+function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost:" + responseObject.errorMessage);
+    }
+}
