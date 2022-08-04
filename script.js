f63527446c025f78f696320ceb6dbd0b946f3db4
Julia Melik <julija.melik@gmail.com>
2022-08-04 14:42:13 +0200

    css

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ function onConnectionLost(responseObject) {
 
 
 function objevuje() {
-    var value = parseInt(document.getElementById('number').value, 10);
+    var value = parseInt(document.getElementById('#number').value, 10);
     value = isNaN(value) ? 0 : value;
     value++;
-    document.getElementById('number').value = value;
+    document.getElementById('#number').value = value;
 }
